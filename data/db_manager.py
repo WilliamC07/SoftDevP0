4da64b54b755441b181f6d612bf459a217aac3fd
Saad Bhuiyan <ssaad00@stuy.edu>
2019-10-22 17:06:40 -0400

    print -> return

--- data/db_manager.py
+++ data/db_manager.py
@@ -14,14 +14,14 @@ def addLogin(inputName, inputPassword): #facilitate adding new login credentials
     c.execute("SELECT * FROM users WHERE name = '%s';" % inputName) #all instances of name in database
     if c.fetchone() is None: #if name is not found in database
         c.execute("INSERT INTO users(name, password) VALUES('%s', '%s');" % (inputName, inputPassword)) #add login credentials
-        print("Successfully added login credentials!")
+        return "Successfully added login credentials!"
     else: 
-        print("Username exists!")
+        return "Username exists!"
     db.commit() #save changes
     db.close() #close database
 
-# addLogin("newAdmin", "newPassword") #test adding login credentials with new name
-# addLogin("admin", "password") #test adding login credentials with existing name
+#print(addLogin("newAdmin", "newPassword")) #test adding login credentials with new name
+#print(addLogin("admin", "password")) #test adding login credentials with existing name
 
 #===================================================================================
 
@@ -30,13 +30,13 @@ def verifyLogin(inputName, inputPassword): #verify login credentials
     c = db.cursor() #facilitate db ops
     c.execute("SELECT * FROM users WHERE name = '%s' AND password = '%s';" % (inputName, inputPassword)) #any rows where the inputs match
     if c.fetchone() is not None: #if login credentials are found in database
-        print("Login credentials verified!")
+        return "Login credentials verified!"
     else: 
-        print("Login credentials not found!")
+        return "Login credentials not found!"
     db.commit() #save changes
     db.close() #close database
 
-# verifyLogin("admin", "password") #test existing login
-# verifyLogin("admin", "fakePassword") #test fake login credentials
+#print(verifyLogin("admin", "password")) #test existing login
+#print(verifyLogin("admin", "fakePassword")) #test fake login credentials
 
 #===================================================================
