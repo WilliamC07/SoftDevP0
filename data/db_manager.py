65fc1ffcb812a923cfa8a8ac52c2b0ab642faaac
Saad Bhuiyan <ssaad00@stuy.edu>
2019-10-22 17:25:44 -0400

    resolved issue of db not saving

--- data/db_manager.py
+++ data/db_manager.py
@@ -14,13 +14,15 @@ def addLogin(inputName, inputPassword): #facilitate adding new login credentials
     c.execute("SELECT * FROM users WHERE name = '%s';" % inputName) #all instances of name in database
     if c.fetchone() is None: #if name is not found in database
         c.execute("INSERT INTO users(name, password) VALUES('%s', '%s');" % (inputName, inputPassword)) #add login credentials
+        db.commit() #save changes
+        db.close() #close database
         return "Successfully added login credentials!"
     else: 
+        db.commit() #save changes
+        db.close() #close database
         return "Username exists!"
-    db.commit() #save changes
-    db.close() #close database
 
-#print(addLogin("newAdmin", "newPassword")) #test adding login credentials with new name
+print(addLogin("newAdmin", "newPassword")) #test adding login credentials with new name
 #print(addLogin("admin", "password")) #test adding login credentials with existing name
 
 #===================================================================================
@@ -30,11 +32,13 @@ def verifyLogin(inputName, inputPassword): #verify login credentials
     c = db.cursor() #facilitate db ops
     c.execute("SELECT * FROM users WHERE name = '%s' AND password = '%s';" % (inputName, inputPassword)) #any rows where the inputs match
     if c.fetchone() is not None: #if login credentials are found in database
+        db.commit() #save changes
+        db.close() #close database
         return "Login credentials verified!"
     else: 
+        db.commit() #save changes
+        db.close() #close database
         return "Login credentials not found!"
-    db.commit() #save changes
-    db.close() #close database
 
 #print(verifyLogin("admin", "password")) #test existing login
 #print(verifyLogin("admin", "fakePassword")) #test fake login credentials
