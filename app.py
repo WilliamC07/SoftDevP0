ef49b8c1fba9dc9eaf6156ac3b19097b933c3ef4
William Cao <wcao00@stuy.edu>
2019-10-24 09:25:31 -0400

    work for tonight

--- app.py
+++ app.py
@@ -12,16 +12,18 @@ app = Flask(__name__)
 #create instance of class Flask
 app.secret_key = os.urandom(32)
 
+
 @app.route("/")
 def root():
     if "username" in session:
         return redirect(url_for("home"))
     return redirect(url_for("login"))
 
+
 @app.route("/login")
 def login():
     if len(request.args) == 2:
-        response = db_manager.verifyLogin(request.args["username"], request.args["password"])
+        response = db_manager.verify_login(request.args["username"], request.args["password"])
         if response == "":
             session["username"] = request.args["username"]
             #session["id"] = <get id from db>
@@ -30,46 +32,51 @@ def login():
             flash(response)
     return render_template("login/login.html")
 
+
 @app.route("/create-account")
 def create_account():
     if len(request.args) == 3:
         if request.args["passwordNew"] != request.args["passwordRepeat"]:
             flash("Passwords don't match, try again")
         else:
-            response = db_manager.addLogin(request.args["username"], request.args["passwordNew"])
+            response = db_manager.add_login(request.args["username"], request.args["passwordNew"])
             if response == "":
                 session["username"] = request.args["username"]
-                #session["id"] = <get id from db>
                 return redirect(url_for("home"))
             else:
                 flash(response)
     return render_template("login/create-account.html")
 
+
 @app.route("/home")
 def home():
     # get username from session
-    # need: function from database to get list of all users, beside the one logged in, with blogs
+    # need: function from database to get list of all usernames with blogs
     return "welcome home - home.html to be added"
+
 
 @app.route("/blogs")
 def blogs():
-    # get username/id from session and get user_id from frontend
-    # need: function from database to get all the blogs of the user with given user_id
+    # get username from session and username of viewing blog from frontend
+    # need: function from database to get all the blogs' title of the user (should be recent first, need list)
     #       function (createBlog) from database
     return ""
 
+
 @app.route("/blogs/entries")
 def entries():
-    # get username/id from session and get blog_id from frontend
+    # get username from session and get blog_id from frontend
     # need: function from database to return list of all entries for the blog
-    #       functions (addEntry, updateEntry, verifyBlogAuthor) from database
+    #       functions (addEntry, updateEntry, is_owner) from database
     #
     return ""
+
 
 @app.route("/logout")
 def logout():
     # pop session, redirect to /login
     return ""
+
 
 if __name__ == "__main__":
     app.debug = True
--- data/db_manager.py
+++ data/db_manager.py
@@ -6,7 +6,7 @@
 
 import sqlite3
 
-def addLogin(inputName, inputPassword): #facilitate adding new login credentials
+def add_login(inputName, inputPassword): #facilitate adding new login credentials
     db = sqlite3.connect("spew.db") #open file
     c = db.cursor() #facilitate db ops
     c.execute("SELECT * FROM users WHERE name = ?;" , inputName) #all instances of name in database
@@ -23,7 +23,7 @@ def addLogin(inputName, inputPassword): #facilitate adding new login credentials
 # addLogin("admin", "password") #test adding login credentials with new name
 # addLogin("admin", "password") #test adding login credentials with existing name
 
-def verifyLogin(inputName, inputPassword): #verify login credentials
+def verify_login(inputName, inputPassword): #verify login credentials
     db = sqlite3.connect("spew.db") #open file
     c = db.cursor() #facilitate db ops
     c.execute("SELECT * FROM users WHERE name = ? AND password = ?;" , (inputName, inputPassword)) #any rows where the inputs match
@@ -36,5 +36,41 @@ def verifyLogin(inputName, inputPassword): #verify login credentials
         db.close() #close database
         return "Login credentials not found!"
 
+
 # verifyLogin("admin", "password") #test existing login
 # verifyLogin("admin", "fakePassword") #test fake login credentials
+
+
+def get_usernames_with_blogs() -> list:
+    pass
+
+
+def get_blogs_for_username(username: str) -> list:
+    pass
+
+
+# return empty string if works, else return error message
+def create_blog_for_username(username: str, blog_title: str) -> str:
+    pass
+
+
+# List of turples (title, content)
+def get_entries_for_blog(blog: int) -> list:
+    pass
+
+
+def is_owner(username: str, blog_id: int) -> bool:
+    pass
+
+
+# return empty string if works, else return error message
+def add_entry(username: str, blog_title: str, blog_content: str) -> str:
+    pass
+
+
+def remove_entry(blog_id: int):
+    pass
+
+
+def replace_entry(blog_id: int, blog_title: str, blog_content: str) -> str:
+    pass
