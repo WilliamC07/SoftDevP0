72d948e4f73e8a03b7cd4c4f82bf5074a5fd4691
Saad Bhuiyan <ssaad00@stuy.edu>
2019-10-24 09:10:39 -0400

    formatting change

--- data/db_manager.py
+++ data/db_manager.py
@@ -6,32 +6,27 @@
 
 import sqlite3
 
-#========================================================================================
-
 def addLogin(inputName, inputPassword): #facilitate adding new login credentials
     db = sqlite3.connect("spew.db") #open file
     c = db.cursor() #facilitate db ops
-    c.execute("SELECT * FROM users WHERE name = '%s';" % inputName) #all instances of name in database
+    c.execute("SELECT * FROM users WHERE name = ?;" , inputName) #all instances of name in database
     if c.fetchone() is None: #if name is not found in database
-        c.execute("INSERT INTO users(name, password) VALUES('%s', '%s');" % (inputName, inputPassword)) #add login credentials
+        c.execute("INSERT INTO users(name, password) VALUES(?, ?);" , (inputName, inputPassword)) #add login credentials
         db.commit() #save changes
         db.close() #close database
         return ""
     else:
         db.commit() #save changes
         db.close() #close database
-        return("Username exists!")
+        return "Username exists!"
 
-
-# addLogin("newAdmin", "newPassword") #test adding login credentials with new name
+# addLogin("admin", "password") #test adding login credentials with new name
 # addLogin("admin", "password") #test adding login credentials with existing name
-
-#===================================================================================
 
 def verifyLogin(inputName, inputPassword): #verify login credentials
     db = sqlite3.connect("spew.db") #open file
     c = db.cursor() #facilitate db ops
-    c.execute("SELECT * FROM users WHERE name = '%s' AND password = '%s';" % (inputName, inputPassword)) #any rows where the inputs match
+    c.execute("SELECT * FROM users WHERE name = ? AND password = ?;" , (inputName, inputPassword)) #any rows where the inputs match
     if c.fetchone() is not None: #if login credentials are found in database
         db.commit() #save changes
         db.close() #close database
@@ -39,10 +34,7 @@ def verifyLogin(inputName, inputPassword): #verify login credentials
     else:
         db.commit() #save changes
         db.close() #close database
-        return("Login credentials not found!")
-
+        return "Login credentials not found!"
 
 # verifyLogin("admin", "password") #test existing login
 # verifyLogin("admin", "fakePassword") #test fake login credentials
-
-#===================================================================
