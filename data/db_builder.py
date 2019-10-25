e81f87456cfeeaafbf2d83156338d0640bb7489d
William Cao <wcao00@stuy.edu>
2019-10-24 22:02:58 -0400

    create table for blog and entries

--- data/db_builder.py
+++ data/db_builder.py
@@ -12,6 +12,8 @@ c = db.cursor()                 #facilitate db ops
 #======================================================================
 
 c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE, password TEXT);") #create table for user login credentials
+c.execute("CREATE TABLE IF NOT EXISTS blogs(blog_id INTEGER PRIMARY KEY AUTOINCREMENT, blog_name TEXT UNIQUE, blog_author_id INTEGER, blog_last_update timestamp, FOREIGN KEY(blog_author_id) REFERENCES users(id));")
+c.execute("CREATE TABLE IF NOT EXISTS entries(entry_id INTEGER PRIMARY KEY AUTOINCREMENT, entry_blog INTEGER, entry_title TEXT, entry_content TEXT, entry_date timestamp, entry_last_update timestamp, FOREIGN KEY(entry_blog) REFERENCES blogs(blog_id));")
 
 #=========================================================================================================================================
 
