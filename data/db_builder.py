cea042f1ecf2f0ab0c1279c8369d543e4f96bb38
Saad Bhuiyan <ssaad00@stuy.edu>
2019-10-28 08:53:53 -0400

    cleaner db_builder

--- data/db_builder.py
+++ data/db_builder.py
@@ -1,21 +1,27 @@
-#===========================================================
+# ===========================================================
 
-#db_builder is to be used to create the database and tables.
+# db_builder is to be used to initialize the database 
+# with the tables for users, blogs, and entries.
 
-#===========================================================
+# ===========================================================
 
 import sqlite3
 
-db = sqlite3.connect("spew.db") #open if file exists, otherwise create
-c = db.cursor()                 #facilitate db ops
+db = sqlite3.connect("spew.db") # open if file exists, otherwise create
+c = db.cursor()                 # facilitate db ops
 
-#======================================================================
+# ======================================================================
 
+# create a users table that stores an id, the user's name, and the user's password
 c.execute("CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY AUTOINCREMENT, user_name TEXT UNIQUE, user_password TEXT);") #create table for user login credentials
+
+# create a blogs table that stores an id, the blog's name, the blog's author, and the last update time of the blog 
 c.execute("CREATE TABLE IF NOT EXISTS blogs(blog_id INTEGER PRIMARY KEY AUTOINCREMENT, blog_name TEXT, blog_author TEXT, blog_last_update DATETIME NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')), FOREIGN KEY(blog_author) REFERENCES users(user_name));") #create table for every blog
+
+# create a entries table that stores an id, the id of the blog to which the entry belongs, the entry's title, the contents of the entry, and the last update time of the entry
 c.execute("CREATE TABLE IF NOT EXISTS entries(entry_id INTEGER PRIMARY KEY AUTOINCREMENT, entry_blog INTEGER, entry_title TEXT, entry_content TEXT, entry_last_update DATETIME NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')), FOREIGN KEY(entry_blog) REFERENCES blogs(blog_id));") #create table for every entry
 
-#=========================================================================================================================================
+# =========================================================================================================================================
 
-db.commit() #save changes
-db.close()  #close database
+db.commit() # save changes
+db.close()  # close database
